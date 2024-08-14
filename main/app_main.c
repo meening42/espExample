57e40069d12653f453ce7cadc41a53ec0148cbca
marko <marko.antoncic@gmail.com>
2024-08-14 19:52:53 +0200

    Changed output to Json form

--- main/app_main.c
+++ main/app_main.c
@@ -285,27 +285,16 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32, base, event_id);
     esp_mqtt_event_handle_t event = event_data;
     esp_mqtt_client_handle_t client = event->client;
-    int msg_id;
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-        msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        esp_mqtt_client_subscribe(client, "example/temperature_request", 1);
         break;
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
         break;
-
     case MQTT_EVENT_SUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
-        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
         break;
     case MQTT_EVENT_UNSUBSCRIBED:
         ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -317,20 +306,16 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
-        if(strcmp (event->topic,"tempRequest") == 0){
+        char requestTopic[32] = "example/temperature_request";
+        if(strncmp (event->topic,requestTopic,event->topic_len ) == 0){
             triggerSrc_t trigger = TRIGGER_MQTT_REQUEST;
             xQueueSendFromISR(evt_queue, &trigger, NULL);
+        }else{
+            printf("I gues not the same :(\n");
         }
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
-            //log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
-            //log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            //log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
-
-        }
         break;
     default:
         ESP_LOGI(TAG, "Other event id:%d", event->event_id);
@@ -369,20 +354,20 @@ static void myTask(void* arg)
             temperature_sensor_get_celsius(temp_sensor, &tsens_value);
             switch(src){
                 case TRIGGER_BTN:                   
-                    snprintf(buffer, sizeof(buffer), "btn pressed, temp = %.1f\n", tsens_value);
+                    snprintf(buffer, sizeof(buffer), "{\"trigger_src\":\"button\", \"temperature\":%.1f}\n", tsens_value);
                     break;
                 case TRIGGER_TIMER:
-                    snprintf(buffer, sizeof(buffer), "timer 5s, temp = %.1f\n", tsens_value);
+                    snprintf(buffer, sizeof(buffer), "{\"trigger_src\":\"timer\", \"temperature\":%.1f}\n", tsens_value);
                     break;
                 case TRIGGER_MQTT_REQUEST:   
-                    snprintf(buffer, sizeof(buffer), "mqtt req., temp = %.1f\n", tsens_value);
+                    snprintf(buffer, sizeof(buffer), "{\"trigger_src\":\"mqtt_request\", \"temperature\":%.1f}\n", tsens_value);
                     break;
                 default:
-                    snprintf(buffer, sizeof(buffer), "unknown trigger, temp = %.1f\n", tsens_value);
+                    snprintf(buffer, sizeof(buffer), "{\"trigger_src\":\"unknown\", \"temperature\":%.1f}\n", tsens_value);
                     break;
             }
             printf("%s",buffer);
-            esp_mqtt_client_publish(client, "temperature", buffer, 0, 1, 1);
+            esp_mqtt_client_publish(client, "example/data", buffer, 0, 1, 1);
         }
     }
 }
@@ -515,13 +500,10 @@ void app_main(void)
     /* Start main application now */
      mqtt_app_start();
 
-    // initialize Btn input
     initBtnGpio();
-
-    //create a queue to handle gpio event from isr
     evt_queue = xQueueCreate(10, sizeof(triggerSrc_t));
 
-    // start 5s timer
+    // start 5s t
     tmr = xTimerCreate("MyTimer", pdMS_TO_TICKS(interval), pdTRUE, ( void * )id, &ping);
     if( xTimerStart(tmr, 10 ) != pdPASS ) {
      printf("Timer start error");
@@ -529,15 +511,6 @@ void app_main(void)
     //start task
     xTaskCreate(myTask, "myTask", 2048, NULL, 10, NULL);
 }
-
-
-
-
-
-
-
-
-
 
 
 static void mqtt_app_start(void)
@@ -552,11 +525,8 @@ static void mqtt_app_start(void)
       },
     }
   };
-
-    ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
     client = esp_mqtt_client_init(&mqtt_cfg);
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
-
 }
